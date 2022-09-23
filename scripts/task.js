8a2e76f7252124c2322bb9ed9ab6f035254ae075
Markov Andrey <andremarkov@icloud.com>
2022-09-23 15:46:14 +0300

    Task 8, 9, 10 complete

--- scripts/task.js
+++ scripts/task.js
@@ -1,5 +1,5 @@
 //Задание 2
-console.log('Задание 2');
+console.log('ЗАДАНИЕ 2');
 let a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18;
 a0 = '  ';
 a1 = 5 % 3;
@@ -39,7 +39,7 @@ console.log(a0 + 'a17) ' + 'false || true = ' + a17);
 a18 = true > 0;
 console.log(a0 + 'a18) ' + 'true > 0 = ' + a18);
 //Задание 3
-console.log('Задание 3');
+console.log('ЗАДАНИЕ 3');
 let width, height, SPryam;
 width = 10;
 height = 23;
@@ -48,17 +48,17 @@ console.log(a0 + 'При высоте прямоугольника '
 + height + 'см и ширине ' + width 
 + 'см, площадь прямоугольника = ' + SPryam + 'кв.см');
 //Задание 4
-console.log('Задание 4');
+console.log('ЗАДАНИЕ 4');
 let VCilindra;
 VCilindra = Math.PI * Math.pow((a7/2), 2) * 10;
 console.log(a0 + 'При высоте цилиндра = 10м и диаметре ' + a7 
 + 'м, объем цилиндра ' + VCilindra.toFixed(2) + 'куб.м');
 //Задание 5
-console.log('Задание 5');
+console.log('ЗАДАНИЕ 5');
 let SKruga = Math.PI * Math.pow(5, 2);
 console.log(a0 + 'При радиусе круга 5см площадь = ' + SKruga.toFixed(2) + 'кв.см');
 //Задание 6
-console.log('Задание 6');
+console.log('ЗАДАНИЕ 6');
 let ATrap, BTrap, HTrap, STrap;
 ATrap = 5;
 BTrap = 7;
@@ -66,11 +66,25 @@ HTrap = 10;
 STrap = (ATrap+BTrap)/2 * HTrap;
 console.log(a0 + 'При радиусе круга 5см площадь = ' + STrap + 'кв.см');
 //Задание 7
-console.log('Задание 7');
+console.log('ЗАДАНИЕ 7');
 let S = 2000000;
 let p = 10 / 1200;
 let n = 60;
 let Pereplata = ((S * p / (1 - Math.pow(1 + p, -n)))*n)-S;
 console.log(a0 + 'При кредите на 5 лет размером ' + S + 'руб. и 10%-ставке переплата составляет ' 
 + Pereplata.toFixed(2) + ' руб.');
-//
+//Задание 8
+console.log('ЗАДАНИЕ 8');
+a = 8;
+b = 3;
+// a+2(x-b)=16
+// b(x+15)=a+6x
+// x+2x+ax+bx=23780
+let x1 = (16-a+2*b)/2;
+console.log('x1 = ' + x1 + '\n');
+
+let x2 = (a-15*b) / (b - 6);
+console.log('x2 = ' + x2 + '\n');
+
+let x3 = 23780 / (3 + a + b);
+console.log('x3 = ' + x3 + '\n');
